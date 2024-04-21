adb023a4dda81c1ec21eab76704f04ea6a469d9c
Harold_Aguirre <haroldantonio@777gmail.com>
2024-04-20 22:32:42 -0600

    api send

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,15 @@
-//import TOKEN from "../config.js";
-
+import TOKEN from "../config.js";
+const ApiURL = TOKEN;
 document.getElementById("contact-role").addEventListener("submit", (ev) => {
   ev.preventDefault();
-
+  const controls = ev.target;
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "text/plain;charset=utf-8");
 
   const raw = JSON.stringify({
-    to: "haroldantonio777@gmail.com",
-    subject: "Prueba",
-    body: "<p>Hola</p>",
+    to: controls.email.value,
+    subject: controls.subject.value,
+    body: controls.message.value,
   });
 
   const requestOptions = {
@@ -17,14 +17,10 @@ document.getElementById("contact-role").addEventListener("submit", (ev) => {
     method: "POST",
     headers: myHeaders,
     body: raw,
-    mode: "cors",
   };
 
-  fetch(
-    "https://script.google.com/macros/s/AKfycbzfwkUv1rmt7jaIBEjZu5D_EJS9Nj775SRld_aOk_uYQJdDyYIs76eDXKqTx2Dlv1QyfA/exec",
-    requestOptions
-  )
-    .then((response) => console.log(response))
+  fetch(ApiURL, requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
     .catch((error) => console.log(error));
 });
-//const ApiURL = TOKEN;
