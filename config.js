be66b108e8dbccaa6a3030205e74c27e1fb87810
Harold_Aguirre <haroldantonio@777gmail.com>
2024-04-21 00:50:03 -0600

    cambios para cv

--- config.js
+++
@@ -1,3 +0,0 @@
-const TOKEN =
-  "https://script.google.com/macros/s/AKfycbzfwkUv1rmt7jaIBEjZu5D_EJS9Nj775SRld_aOk_uYQJdDyYIs76eDXKqTx2Dlv1QyfA/exec";
-export default TOKEN;
--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,7 @@
-import TOKEN from "../config.js";
-const ApiURL = TOKEN;
 document.getElementById("contact-role").addEventListener(
   "submit",
   (ev) => {
     ev.preventDefault();
-    const controls = ev.target;
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "text/plain;charset=utf-8");
-
-    const raw = JSON.stringify({
-      to: controls.email.value,
-      subject: controls.subject.value,
-      body: controls.message.value,
-    });
-
-    const requestOptions = {
-      redirect: "follow",
-      method: "POST",
-      headers: myHeaders,
-      body: raw,
-    };
-
-    fetch(ApiURL, requestOptions)
-      .then((response) => response.text())
-      .then((result) => console.log("Mensaje enviado"))
-      .catch((error) => console.log(error));
   },
   false
 );
