cbcc659b8bcc0e92efa8dafcb0e1229d487c6568
Harold_Aguirre <haroldantonio@777gmail.com>
2024-04-20 23:04:59 -0600

    prevenir envio de recarga

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,30 @@
 import TOKEN from "../config.js";
 const ApiURL = TOKEN;
-document.getElementById("contact-role").addEventListener("submit", (ev) => {
-  ev.preventDefault();
-  const controls = ev.target;
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "text/plain;charset=utf-8");
+document.getElementById("contact-role").addEventListener(
+  "submit",
+  (ev) => {
+    ev.preventDefault();
+    const controls = ev.target;
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "text/plain;charset=utf-8");
 
-  const raw = JSON.stringify({
-    to: controls.email.value,
-    subject: controls.subject.value,
-    body: controls.message.value,
-  });
+    const raw = JSON.stringify({
+      to: controls.email.value,
+      subject: controls.subject.value,
+      body: controls.message.value,
+    });
 
-  const requestOptions = {
-    redirect: "follow",
-    method: "POST",
-    headers: myHeaders,
-    body: raw,
-  };
+    const requestOptions = {
+      redirect: "follow",
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+    };
 
-  fetch(ApiURL, requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log(error));
-});
+    fetch(ApiURL, requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log("Mensaje enviado"))
+      .catch((error) => console.log(error));
+  },
+  false
+);
